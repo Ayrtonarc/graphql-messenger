4590ed339a04a87549d2c4a505207514188da21b
General Krypton <ayrton.arc@gmail.com>
2022-10-02 20:51:42 -0700

    generando modelo de mensajes

---
+++ database/migrations/20221003035108-create-message.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Messages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      text: {
+        type: Sequelize.STRING
+      },
+      viewed: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        type: Sequelize.STRING
+      },
+      emmiter: {
+        type: Sequelize.STRING
+      },
+      receiver: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Messages');
+  }
+};
